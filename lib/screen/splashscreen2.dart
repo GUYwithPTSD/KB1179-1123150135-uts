d6109a5087f2540d738ee4c5d67156bf31188440
GUYwithPTSD <ivandarma3000@gmail.com>
2025-11-08 00:00:05 +0700

    membuat splashscreen2

---
+++ lib/screen/splashscreen2.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class Splashscreen1 extends StatelessWidget {
+  const Splashscreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Text(
+          "Made by : Ivan (1123150135)",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 12, color: Colors.grey),
+        ),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 50),
+            Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.grey,
+                image: DecorationImage(
+                  image: AssetImage("./assets/images/profile1.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Text(
+              'Welcome',
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              textAlign: TextAlign.center,
+              'Forget to bring your wallet \n while you are shopping',
+              style: TextStyle(
+                fontSize: 10.0,
+                color: Colors.deepPurple,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 5,
+                  width: 5,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                SizedBox(width: 3.0),
+                Container(
+                  height: 5,
+                  width: 5,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                SizedBox(width: 3.0),
+                Container(
+                  height: 5,
+                  width: 5,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey[700],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 100.0),
+            Container(
+              margin: EdgeInsets.only(left: 40, right: 40, bottom: 30),
+              child: SizedBox(
+                height: 40.0,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.pinkAccent,
+                  ),
+                  child: Text(
+                    'Continue',
+                    style: TextStyle(fontSize: 15.0, color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
